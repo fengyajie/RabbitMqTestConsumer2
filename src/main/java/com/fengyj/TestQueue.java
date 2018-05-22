19981dbd3ac930b684f1458f92ba795ea0cfd303
lijc <lijc0208@163.com>
2018-05-22 17:09:06 +0800

    xxx

---
+++ src/main/java/com/fengyj/TestQueue.java
@@ -0,0 +1,31 @@
+package com.fengyj;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+    "classpath:application-mq.xml"})
+
+public class TestQueue{
+
+    @Autowired
+    RabbitTemplate rabbit;
+    
+    @Autowired
+    QueueListener  queueListener;
+
+    @Test
+    public void testSendString() throws Exception {
+    	try {
+    	rabbit.receiveAndConvert("xxx");
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+
+}
