7a0d0c7e1362d45a97d64f929b81f5d98b237298
lijc <lijc0208@163.com>
2018-05-25 17:38:27 +0800

    手动配置

--- src/main/java/com/fengyj/QueueListener.java
+++ src/main/java/com/fengyj/QueueListener.java
@@ -12,6 +12,12 @@ public class QueueListener implements ChannelAwareMessageListener {
 
 	public void onMessage(Message message, Channel channel) throws Exception {
 		
+		
+		/**
+		 * channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); // 消息的标识，false只确认当前一个消息收到，true确认所有consumer获得的消息
+             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true); // ack返回false，并重新回到队列，api里面解释得很清楚
+           channel.basicReject(message.getMessageProperties().getDeliveryTag(), true); // 拒绝消息
+		 */
 		//手动确认
 		channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
 		
